75d8e7177ade55748256c1e21ff88b384dbf9327
Stephanie Ramos <ramos.n.stephanie@outlook.com>
2026-09-04 16:23:38 -0700

    store the MapLibre map object in React State

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import JourneyMap from "./components/Map/JourneyMap"
+import { useState } from "react";
+import { Map } from "maplibre-gl";
+
+import JourneyMap from "./components/Map/JourneyMap";
 import BeginningSection from "./components/Intro/BeginningSection";
 
 function App() {
+  // A function that updates map 
+  const [map, setMap] = useState<Map | null>(null);
+
   return (
     // Here, we use the main tag to identify the central and unique core content of the webpage
     <main className="app">
-      <BeginningSection />
-      <JourneyMap />
+      {/* BeginningSection doesnt have a map property */}
+      <BeginningSection map={map}/>
+      <JourneyMap onMapReady={setMap}/>
     </main>
   );
 }
